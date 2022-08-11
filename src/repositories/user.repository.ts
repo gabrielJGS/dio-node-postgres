e99930a1eb6c85cdf9f970630bbc333eea12b112
gabrielJGS <gabrielftjose@gmail.com>
2022-08-11 15:16:55 -0300

    feat: user crud with basic error handling

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,5 +1,6 @@
 import db from "../db";
 import User from "../models/user.model";
+import DatabaseError from "../errors/database.error.model";
 
 class UserRepository {
   async findAllUsers(): Promise<User[]> {
@@ -8,6 +9,51 @@ class UserRepository {
 
     const { rows } = await db.query<User>(query);
     return rows || [];
+  }
+
+  async findById(uuid: string): Promise<User> {
+    try {
+      const query = `SELECT uuid, username
+    FROM application_user
+    WHERE uuid = $1;`;
+      const values = [uuid];
+
+      const { rows } = await db.query<User>(query, values);
+      const [user] = rows;
+      return user;
+    } catch (error) {
+      throw new DatabaseError("Erro na consulta por id", error);
+    }
+  }
+
+  async create(user: User): Promise<string> {
+    const query = `INSERT INTO application_user (username, password)
+    values($1, crypt($2, 'my_salt'))
+    RETURNING uuid;`;
+    const values = [user.username, user.password];
+
+    const { rows } = await db.query<{ uuid: string }>(query, values);
+    const [newUser] = rows;
+    return newUser.uuid;
+  }
+
+  async update(user: User): Promise<void> {
+    const query = `UPDATE application_user
+    SET
+     username = $1,
+     password = crypt($2, 'my_salt')
+    WHERE uuid = $3;`;
+    const values = [user.username, user.password, user.uuid];
+
+    await db.query(query, values);
+  }
+
+  async remove(uuid: string): Promise<void> {
+    const query = `DELETE FROM application_user
+    WHERE uuid = $1;`;
+    const values = [uuid];
+
+    await db.query(query, values);
   }
 }
 
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
+import DatabaseError from "../errors/database.error.model";
 import userRepository from "../repositories/user.repository";
 
 const usersRoute = Router();
@@ -13,33 +14,51 @@ usersRoute.get(
 );
 
 usersRoute.get(
-  "/users:uuid",
-  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
-    const uuid = req.params;
-    res.status(StatusCodes.OK).send(uuid);
+  "/users/:uuid",
+  async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    try {
+      const uuid = req.params.uuid;
+      const user = await userRepository.findById(uuid);
+      res.status(StatusCodes.OK).send(user);
+    } catch (error) {
+      if (error instanceof DatabaseError) {
+        res.status(StatusCodes.BAD_REQUEST).send(error);
+      } else {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+      }
+    }
   }
 );
 
-usersRoute.post("/users", (req: Request, res: Response, next: NextFunction) => {
-  const newUser = req.body;
-  res.status(StatusCodes.CREATED).send(newUser);
-});
+usersRoute.post(
+  "/users",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+    const uuid = await userRepository.create(newUser);
+
+    res.status(StatusCodes.CREATED).send(uuid);
+  }
+);
 
 usersRoute.put(
-  "/users:uuid",
-  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
-    const uuid = req.params;
+  "/users/:uuid",
+  async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
     const modifiedUser = req.body;
+    modifiedUser.uuid = uuid;
 
-    res.status(StatusCodes.OK).send(modifiedUser);
+    await userRepository.update(modifiedUser);
+
+    res.status(StatusCodes.OK).send();
   }
 );
 
 usersRoute.delete(
-  "/users:uuid",
-  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
-    const uuid = req.params;
-    res.status(StatusCodes.OK).send(uuid);
+  "/users/:uuid",
+  async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    await userRepository.remove(uuid);
+    res.status(StatusCodes.OK).send();
   }
 );
 
