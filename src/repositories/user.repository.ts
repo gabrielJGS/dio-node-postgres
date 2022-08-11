b3c2f8781a69821a8aac757baa0810f026bfdde2
gabrielJGS <gabrielftjose@gmail.com>
2022-08-11 14:31:53 -0300

    feat: db connection and basic user query all

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,14 @@
+import db from "../db";
+import User from "../models/user.model";
+
+class UserRepository {
+  async findAllUsers(): Promise<User[]> {
+    const query = `SELECT uuid, username
+    FROM application_user;`;
+
+    const { rows } = await db.query<User>(query);
+    return rows || [];
+  }
+}
+
+export default new UserRepository();
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,12 +1,16 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
+import userRepository from "../repositories/user.repository";
 
 const usersRoute = Router();
 
-usersRoute.get("/users", (req: Request, res: Response, next: NextFunction) => {
-  const users = [{ userName: "Gabriel" }];
-  res.status(StatusCodes.OK).send(users);
-});
+usersRoute.get(
+  "/users",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const users = await userRepository.findAllUsers();
+    res.status(StatusCodes.OK).send(users);
+  }
+);
 
 usersRoute.get(
   "/users:uuid",
