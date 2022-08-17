eb15e35322e94a54957827a4f8449a153175658d
gabrielJGS <gabrielftjose@gmail.com>
2022-08-17 10:49:20 -0300

    fix: update code to adapt to typeorm

--- src/db.ts
+++
@@ -1,7 +0,0 @@
-import { Pool } from "pg";
-
-const connectionString = "postgres://postgres:postgrespw@localhost:49153/node_user";
-
-const db = new Pool({ connectionString });
-
-export default db;
